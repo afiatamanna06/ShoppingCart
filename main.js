790018d7f8255710394efa6df26e46b906b8fa04
afiatamanna06 <afiatamanna06@gmail.com>
2020-09-18 20:19:31 +0600

    main.js updated

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let carts = document.querySelectorAll(".add-cart");
 
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        console.log("added to cart");
+        cartNumbers();
     })
+}
+
+function cartNumbers() {
+    localStorage.setItem("cartNumbers", 1);
 }
