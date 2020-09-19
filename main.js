a33c2611771bd2649190598ae3d6c108b1f40cb6
afiatamanna06 <afiatamanna06@gmail.com>
2020-09-19 20:07:21 +0600

    main.js updated

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 let carts = document.querySelectorAll(".add-cart");
 
+let products = [{
+        name: "Long Dress",
+        tag: "long_dress",
+        price: 18,
+        inCart: 0
+    },
+    {
+        name: "Ladies Shoe",
+        tag: "ladies_shoe",
+        price: 30,
+        inCart: 0
+    },
+    {
+        name: "Ladies Coat",
+        tag: "ladies_coat",
+        price: 65,
+        inCart: 0
+    },
+    {
+        name: "Ladies Pant",
+        tag: "ladies_pant",
+        price: 35,
+        inCart: 0
+    }
+]
+
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
         cartNumbers();
