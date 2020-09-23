ba3f14b72e4cd8148821a67e524b9fcbcc419884
afiatamanna06 <afiatamanna06@gmail.com>
2020-09-23 19:46:57 +0600

    main.js updated

--- main.js
+++ main.js
@@ -56,6 +56,12 @@ function setItems() {
     cartItems = JSON.parse(cartItems);
 
     if (cartItems != null) {
+        if (cartItems[product.tag] != undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product;
+            }
+        }
         cartItems[product.tag].inCart += 1;
     } else {
         product.inCart = 1;
